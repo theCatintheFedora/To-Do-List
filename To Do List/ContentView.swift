fa17149b9e6bbc50164a24cb788370b602abb006
Timothy Price <theCatintheFedora1367@gmail.com>
2021-02-10 10:49:14 -0600

    I completed the code and also forgot to capitalize an S lol

--- To Do List/ContentView.swift
+++ To Do List/ContentView.swift
@@ -8,9 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var things = ["First Thing", "Second Thing", "Third Thing", "Fourth Thing", "Fifth Thing"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(things, id: \.self) { thing in
+                    Text(thing)
+                }
+                .onMove(perform: {
+                    indices, newOffset in things.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { indexSet in things.remove(atOffsets: indexSet)
+                })
+            }
+            .navigationBarTitle("Things")
+            .navigationBarItems(leading: EditButton())
+        }
     }
 }
 
