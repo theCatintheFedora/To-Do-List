08f224808bd8ed45a730e42343b47b209c320083
Timothy Price <theCatintheFedora1367@gmail.com>
2021-02-11 10:28:59 -0600

    Created Custom To Do Class

--- To Do List/ContentView.swift
+++ To Do List/ContentView.swift
@@ -8,22 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var toDoItems =
-        [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
-         ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
-         ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
+    @ObservedObject var toDoList = ToDoList()
     
     var body: some View {
         NavigationView {
             List {
-                ForEach(toDoItems) { item in
-                    Text(item.description)
+                ForEach(toDoList.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.priority)
+                                .font(.headline)
+                            Text(item.description)
+                        }
+                        Spacer()
+                        Text(item.dueDate, style: .date)
+                    }
                 }
                 .onMove(perform: {
-                    indices, newOffset in toDoItems.move(fromOffsets: indices, toOffset: newOffset)
+                    indices, newOffset in toDoList.items.move(fromOffsets: indices, toOffset: newOffset)
                 })
-                
-                .onDelete(perform: { indexSet in toDoItems.remove(atOffsets: indexSet)
+                .onDelete(perform: { indexSet in toDoList.items.remove(atOffsets: indexSet)
                 })
             }
             .navigationBarTitle("To Do List")
@@ -31,7 +35,6 @@ struct ContentView: View {
         }
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ To Do List/ToDoList.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoList.swift
+//  To Do List
+//
+//  Created by Student on 2/11/21.
+//
+
+
+
+import Foundation
+
+class ToDoList: ObservableObject {
+    @Published var items = [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
+                            ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
+                            ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
+}
