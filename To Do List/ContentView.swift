056c02886d8aa89a87a3dea70c84afb49d0c7723
Timothy Price <theCatintheFedora1367@gmail.com>
2021-02-11 10:15:02 -0600

    Created initial to do list

--- To Do List/ContentView.swift
+++ To Do List/ContentView.swift
@@ -8,28 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var things = ["First Thing", "Second Thing", "Third Thing", "Fourth Thing", "Fifth Thing"]
+    @State var toDoItems =
+        [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
+         ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
+         ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
     
     var body: some View {
         NavigationView {
             List {
-                ForEach(things, id: \.self) { thing in
-                    Text(thing)
+                ForEach(toDoItems) { item in
+                    Text(item.description)
                 }
                 .onMove(perform: {
-                    indices, newOffset in things.move(fromOffsets: indices, toOffset: newOffset)
+                    indices, newOffset in toDoItems.move(fromOffsets: indices, toOffset: newOffset)
                 })
-                .onDelete(perform: { indexSet in things.remove(atOffsets: indexSet)
+                
+                .onDelete(perform: { indexSet in toDoItems.remove(atOffsets: indexSet)
                 })
             }
-            .navigationBarTitle("Things")
+            .navigationBarTitle("To Do List")
             .navigationBarItems(leading: EditButton())
         }
     }
 }
 
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+struct ToDoItem: Identifiable {
+    var id = UUID()
+    var priority = String()
+    var description = String()
+    var dueDate = Date()
 }
